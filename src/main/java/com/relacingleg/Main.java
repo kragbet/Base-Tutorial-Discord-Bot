e81e9c9603c511fec8c458a9152ea950b21eaac3
Tatchy Kragbe <kragbet@gmail.com>
2024-09-26 20:31:28 -0400

    initial commit

---
+++ src/main/java/com/relacingleg/Main.java
@@ -0,0 +1,13 @@
+package com.relacingleg;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+
+    public static void main(String[] args) throws LoginException {
+        JDA jda = JDABuilder.createDefault( "").build();
+    }
+}
